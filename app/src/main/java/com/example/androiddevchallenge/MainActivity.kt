11c6b560f776e29ed1377b03357cfb4243a4394e
AnhVu <tuananh.vu@foody.vn>
2021-02-26 10:48:36 +0700

    improve details screen and update screenshot

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -20,22 +20,27 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -112,13 +117,28 @@ fun PuppyRow(puppy: Puppy, navController: NavController) {
 @Composable
 fun PuppyDetail(id: Int) {
     val puppy = puppies.find { it.id == id } ?: puppies[0]
-    Column {
-        Image(
-            bitmap = ImageBitmap.imageResource(id = puppy.photo),
-            modifier = Modifier.fillMaxWidth().aspectRatio(1.0f),
-            contentDescription = null
-        )
-        Text(text = puppy.desc, modifier = Modifier.padding(10.dp))
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.align(Alignment.TopCenter)) {
+            Image(
+                bitmap = ImageBitmap.imageResource(id = puppy.photo),
+                modifier = Modifier.fillMaxWidth().aspectRatio(1.0f),
+                contentDescription = null
+            )
+            Column(modifier = Modifier.padding(10.dp)) {
+                Text(
+                    text = puppy.name,
+                    fontSize = 21.sp,
+                    modifier = Modifier.padding(vertical = 15.dp)
+                )
+                Text(text = puppy.desc)
+            }
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier.align(Alignment.BottomCenter).padding(10.dp)
+        ) {
+            Text(text = "Give a bone")
+        }
     }
 }
 
