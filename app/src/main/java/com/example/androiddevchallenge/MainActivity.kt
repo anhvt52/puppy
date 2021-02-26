2cb42fe003e115f5e6fc082b69a5efa878180f70
AnhVu <tuananh.vu@foody.vn>
2021-02-26 10:04:52 +0700

    implement puppy list and puppy details screens

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,44 +18,114 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.androiddevchallenge.ui.theme.MyTheme
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
+import com.example.androiddevchallenge.data.Puppy
+import com.example.androiddevchallenge.data.puppies
+import com.example.androiddevchallenge.ui.theme.PuppyAdoptionTheme
+import com.example.androiddevchallenge.ui.theme.shapes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyTheme {
-                MyApp()
+            val navController = rememberNavController()
+            PuppyAdoptionTheme {
+                NavHost(navController = navController, startDestination = "home") {
+                    composable("home") {
+                        PuppyList(puppies = puppies, navController = navController)
+                    }
+                    composable(
+                        "puppy/{puppyId}",
+                        arguments = listOf(
+                            navArgument("puppyId") {
+                                type = NavType.IntType
+                            }
+                        )
+                    ) { backStackEntry ->
+                        PuppyDetail(id = backStackEntry.arguments?.getInt("puppyId") ?: 0)
+                    }
+                }
             }
         }
     }
 }
 
-// Start building your app here!
 @Composable
-fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+fun PuppyList(puppies: List<Puppy>, navController: NavController) {
+    Scaffold {
+        LazyColumn(modifier = Modifier.padding(10.dp)) {
+            items(puppies.size) { index ->
+                PuppyRow(puppies[index], navController)
+            }
+        }
     }
 }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
+fun PuppyRow(puppy: Puppy, navController: NavController) {
+    Card(
+        modifier = Modifier.fillMaxWidth().padding(5.dp).clickable {
+            navController.navigate("puppy/${puppy.id}")
+        },
+        shape = shapes.medium
+    ) {
+        Row {
+            Image(
+                bitmap = ImageBitmap.imageResource(id = puppy.photo),
+                modifier = Modifier.size(90.dp),
+                contentDescription = null
+            )
+            Column(
+                modifier = Modifier.padding(top = 10.dp, start = 10.dp)
+            ) {
+                Text(puppy.name)
+                Text(puppy.gender)
+            }
+        }
     }
 }
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
+fun PuppyDetail(id: Int) {
+    val puppy = puppies.find { it.id == id } ?: puppies[0]
+    Column {
+        Image(
+            bitmap = ImageBitmap.imageResource(id = puppy.photo),
+            modifier = Modifier.fillMaxWidth().aspectRatio(1.0f),
+            contentDescription = null
+        )
+        Text(text = puppy.desc, modifier = Modifier.padding(10.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PuppyAdoptionTheme {
+        PuppyDetail(10)
     }
 }
---
+++ app/src/main/java/com/example/androiddevchallenge/data/PuppyListProvider.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androiddevchallenge.data
+
+import com.example.androiddevchallenge.R
+
+val puppies = listOf<Puppy>(
+    Puppy(
+        1,
+        "Rex",
+        "male",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa.",
+        R.drawable.puppy1
+    ),
+    Puppy(
+        2,
+        "Sophie",
+        "female",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa.",
+        R.drawable.puppy2
+    ),
+    Puppy(
+        3,
+        "Bella",
+        "female",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa.",
+        R.drawable.puppy3
+    ),
+    Puppy(4, "Katniss", "male", "abcdef", R.drawable.puppy4),
+    Puppy(5, "Oprah", "male", "abcdef", R.drawable.puppy5),
+    Puppy(6, "Madonna", "male", "abcdef", R.drawable.puppy6),
+    Puppy(7, "Rex", "male", "abcdef", R.drawable.puppy1),
+    Puppy(8, "Sophie", "female", "abcdef", R.drawable.puppy2),
+    Puppy(9, "Bella", "female", "abcdef", R.drawable.puppy3),
+    Puppy(10, "Katniss", "male", "abcdef", R.drawable.puppy4),
+    Puppy(11, "Oprah", "male", "abcdef", R.drawable.puppy5),
+    Puppy(12, "Madonna", "male", "abcdef", R.drawable.puppy6)
+)
